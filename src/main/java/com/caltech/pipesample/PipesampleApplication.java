6e32c91f33da18b543ae9d514e6e325be1134dfa
student <komalrast2411@gmail.com>
2021-04-24 16:19:12 +0000

    pipeline sample

---
+++ src/main/java/com/caltech/pipesample/PipesampleApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.pipesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipesampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipesampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/pipesample/PipesampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.pipesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipesampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
